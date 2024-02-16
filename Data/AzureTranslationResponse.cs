77b9bf079764341d163c48bb3e34cb20e4282160
tkefauver <tkefauver@gmail.com>
2024-02-16 10:53:42 -0500

    Add project files.

---
+++ Data/AzureTranslationResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AzureTextTranslator {
+    public class AzureTranslationResponse {
+        public DetectedLanguage detectedLanguage { get; set; }
+        public List<Translation> translations { get; set; }
+    }
+    public class DetectedLanguage {
+        public string language { get; set; }
+        public string score { get; set; }
+    }
+
+
+    public class Translation {
+        public string text { get; set; }
+        public string to { get; set; }
+    }
+}
